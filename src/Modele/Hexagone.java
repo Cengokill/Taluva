84839e2dda0480122cfe9d9ccc1b97003e91deef
Killian Cengo <cengokillian@hotmail.com>
2023-04-25 18:27:24 +0200

    Ajouts Hexagone

---
+++ src/Modele/Hexagone.java
@@ -0,0 +1,19 @@
+package Modele;
+
+public class Hexagone {
+    private int x;
+    private int y;
+
+    public Hexagone(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
