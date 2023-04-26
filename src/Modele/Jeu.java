d6ba11111b57284616d9aa350a252f09d5e2a757
DyaZix-Code <79577721+DyaZix-Code@users.noreply.github.com>
2023-04-26 15:28:49 +0200

    Pioche sans Tuile

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import Controleur.ControleurMediateur;
 import Patterns.Observable;
 
+import java.util.Collections;
+
 public class Jeu extends Observable {
     Plateau plateau;
     Joueur joueur1, joueur2;
@@ -15,14 +17,27 @@ public class Jeu extends Observable {
 
     byte[] tuile_a_poser = new byte[2];
 
+    List <Tuile> pioche;
+    private static int TAILLE_PIOCHE = 24;
+
     public Jeu(Parametres p){
+        pioche = new LinkedList<Tuile>();
         lancePartie();
     }
 
     public void lancePartie(){
+        initPioche();
+
         plateau = new Plateau();
         tuile_a_poser[0] = Hexagone.GRASS;
         tuile_a_poser[1] = Hexagone.WATER;
+    }
+
+    public void initPioche(){//24 tuiles
+        for(int i=0;i<TAILLE_PIOCHE;i++){
+            pioche.add(new Tuile());
+        }
+        Collections.shuffle(pioche);
     }
 
     public byte[] getTuilesAPoser() {
