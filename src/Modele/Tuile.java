ccb5ad8f9efc56879b7a974f96d5c22f1d50c632
Killian Cengo <cengokillian@hotmail.com>
2023-04-26 18:48:14 +0200

    Ajout à Tuile numero = ((int)(Math.random() * 3) + 1);

--- src/Modele/Tuile.java
+++ src/Modele/Tuile.java
@@ -2,6 +2,7 @@ package Modele;
 
 public class Tuile {
     byte terrain1, terrain2, terrain_volcan;
+    int numero;
 
     public Tuile(){
         //nombre aléatoire entre 1 et 6
@@ -10,5 +11,7 @@ public class Tuile {
         terrain1 = (byte) ((int)(Math.random() * 4) + 1);
         terrain2 = (byte) ((int)(Math.random() * 4) + 1);
         terrain_volcan = Hexagone.VOLCAN;
+        //numero aléatoire entre 0 et 2
+        numero = ((int)(Math.random() * 3) + 1);
     }
 }
