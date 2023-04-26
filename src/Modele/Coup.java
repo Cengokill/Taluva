5c570a87b6f7d76876ba572372e30b47df49b203
DyaZix-Code <79577721+DyaZix-Code@users.noreply.github.com>
2023-04-26 14:19:38 +0200

    Update Coup

--- src/Modele/Coup.java
+++ src/Modele/Coup.java
@@ -3,6 +3,12 @@ package Modele;
 public class Coup {
     byte type;//0 : placer une tuile, 1 : placer un bâtiment
 
+    int volcan_x;
+    int volcan_y;
+    int tuile2_x;
+    int tuile2_y;
+    int tuile3_x;
+    int tuile3_y;
     public Coup(byte type) {
         this.type = type;
     }
