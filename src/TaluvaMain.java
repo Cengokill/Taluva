67cae7fc79947a694aeca170972ec85b0599b61b
DyaZix-Code <blancosacha38@gmail.com>
2023-05-01 09:33:16 +0200

    Amélioration performances graphiques

--- src/TaluvaMain.java
+++ src/TaluvaMain.java
@@ -8,7 +8,6 @@ import Vue.TEngine;
 public class TaluvaMain {
     public static void main(String[] args) {
 
-        ImageLoader.loadImages();
         Jeu jeu = new Jeu(null);
         ControleurMediateur controler = new ControleurMediateur(jeu);
         EchoServeur serveur = new EchoServeur(6500);
@@ -18,5 +17,6 @@ public class TaluvaMain {
         t.start();
 
         TEngine fenetre = new TEngine(jeu, controler);
+        ImageLoader.loadImages();
     }
 }
