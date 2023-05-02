366c10e37439486077e1e6fac8ada1039ebe8ed5
DyaZix-Code <blancosacha38@gmail.com>
2023-05-02 23:32:36 +0200

    broutilles

--- src/Modele/Tuile.java
+++ src/Modele/Tuile.java
@@ -1,20 +1,18 @@
 package Modele;
 
-import Modele.Hexagone;
-
 public class Tuile {
-    final byte terrain0;
-    final byte terrain1;
-    final byte terrain_volcan;
+    final byte biome0;
+    final byte biome1;
+    final byte biomeVolcan;
     final int numero0;
     final int numero1;
     final int numero2;
 
     public Tuile(){
         //nombre aléatoire entre 1 et 6
-        terrain0 = (byte) ((int)(Math.random() * 4) + 1);
-        terrain1 = (byte) ((int)(Math.random() * 4) + 1);
-        terrain_volcan = Hexagone.VOLCAN;
+        biome0 = (byte) ((int)(Math.random() * 4) + 1);
+        biome1 = (byte) ((int)(Math.random() * 4) + 1);
+        biomeVolcan = Hexagone.VOLCAN;
         //numero aléatoire entre 0 et 2
         numero0 = ((int)(Math.random() * 3));
         numero1 = ((int)(Math.random() * 3));
