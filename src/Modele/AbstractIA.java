7c8b9c619bbe78ff226aa6a0d9925fd42c75cff3
Killian Cengo <cengokillian@hotmail.com>
2023-05-05 10:35:18 +0200

    Ajout récupération pioche de l'IA

--- src/Modele/AbstractIA.java
+++ src/Modele/AbstractIA.java
@@ -31,37 +31,8 @@ public class AbstractIA implements Runnable{//une AbstractIA est ex�cut�e pa
         return resultat;
     }
 
-    public void setPioche(){//24 tuiles fixes définies
-        byte desert = DESERT;
-        byte foret = FORET;
-        byte prairie = GRASS;
-        byte montagne = MONTAGNE;
-        byte lac = LAC;
-        pioche.add(new Tuile(desert, desert));
-        pioche.add(new Tuile(prairie, prairie));
-        pioche.add(new Tuile(foret, foret));
-        pioche.add(new Tuile(montagne, montagne));
-        pioche.add(new Tuile(lac, lac));
-        for(int i=0; i<2; i++){
-            pioche.add(new Tuile(lac, prairie));
-            pioche.add(new Tuile(lac, desert));
-            pioche.add(new Tuile(lac, montagne));
-        }
-        for(int i=0; i<3; i++){
-            pioche.add(new Tuile(lac, foret));
-            pioche.add(new Tuile(montagne, desert));
-        }
-        for(int i=0; i<4; i++){
-            pioche.add(new Tuile(montagne, prairie));
-            pioche.add(new Tuile(montagne, foret));
-            pioche.add(new Tuile(prairie, desert));
-        }
-        for(int i=0; i<8; i++){
-            pioche.add(new Tuile(desert, foret));
-        }
-        for(int i=0; i<11; i++){
-            pioche.add(new Tuile(prairie, foret));
-        }
+    public void getPioche(){//24 tuiles fixes définies
+        pioche = jeu.getPioche();
     }
 
     public Coup joue() {
