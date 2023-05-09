4940a4cecbed5885cdff04c21c90928d890e0daa
DyaZix-Code <blancosacha38@gmail.com>
2023-05-09 13:29:35 +0200

    Code plus lisible

--- src/Modele/IA/InstancePlateau.java
+++ src/Modele/IA/InstancePlateau.java
@@ -10,25 +10,54 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 public class InstancePlateau {
-    public Integer piocheJeu;
+    public Integer pioche;
     public Integer plateau;
 
-    public InstancePlateau(ArrayList<Tuile> pioche, Plateau pl){
-        //récupère la pioche sous forme Serializable
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ByteArrayOutputStream bos2 = new ByteArrayOutputStream();
+    public InstancePlateau(ArrayList<Tuile> pioche, Plateau plateau){
+        // Récupère la pioche et le plateau sous forme Serializable
+
+        ByteArrayOutputStream piocheByte = new ByteArrayOutputStream();
+        ByteArrayOutputStream plateauByte = new ByteArrayOutputStream();
+
+        creerObjetPioche(piocheByte);
+        creerObjetPlateau(plateau, plateauByte);
+
+        byte[] piocheByteData = convertirEnByte(piocheByte);
+        byte[] plateauByteData = convertirEnByte(plateauByte);
+
+        this.pioche = readFromByte(piocheByteData);
+        this.plateau = readFromByte(plateauByteData);
+    }
+
+    private int readFromByte(byte[] objectData) {
+        return ByteBuffer.wrap(objectData).getInt();
+    }
+
+    private byte[] convertirEnByte(ByteArrayOutputStream piocheByte) {
+        byte[] piocheByteData = piocheByte.toByteArray();
+        return piocheByteData;
+    }
+
+    private void creerObjetPlateau(Plateau plateau, ByteArrayOutputStream plateauByte) {
         try {
-            ObjectOutputStream oos = new ObjectOutputStream(bos);
-            ObjectOutputStream oos2 = new ObjectOutputStream(bos2);
-            oos.writeObject(pioche);
-            oos2.writeObject(pl);
+            ObjectOutputStream plateauStream = new ObjectOutputStream(plateauByte);
+
+
+            plateauStream.writeObject(plateau);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        byte[] data1 = bos.toByteArray();
-        byte[] data2 = bos2.toByteArray();
-        piocheJeu = ByteBuffer.wrap(data1).getInt();
-        plateau = ByteBuffer.wrap(data2).getInt();
+    }
+
+    private void creerObjetPioche(ByteArrayOutputStream piocheByte) {
+        try {
+            ObjectOutputStream piocheStream = new ObjectOutputStream(piocheByte);
+
+
+            piocheStream.writeObject(piocheStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
