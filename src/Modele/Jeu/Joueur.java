5074fabf6ff6f75658f46957c31f1a2331de3db9
JeejaFR <jeremy.defuentes@gmail.com>
2023-05-19 17:16:42 +0200

    IA intelligente

--- src/Modele/Jeu/Joueur.java
+++ src/Modele/Jeu/Joueur.java
@@ -1,11 +1,13 @@
 package Modele.Jeu;
 
+import Structures.Village;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Joueur implements Serializable {
     public final static byte HUMAIN = 0;
